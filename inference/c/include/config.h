cf62c374f8f849a59cc40f5105d7e089b2fd8e77
Vishwambhara R Hebbalalu <vishwambharah@gmail.com>
2026-03-18 11:40:54 +0530

    add config and model header files

---
+++ inference/c/include/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define INPUT_LEN 256
+#define IN_CH 1
+
+#define C1_OUT 16
+#define C1_K 5
+
+#define C2_OUT 32
+#define C2_K 5
+
+#define NUM_CLASSES 6
+
+#endif
---
+++ inference/c/include/model.h
@@ -0,0 +1,6 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+void forward(float *input, float *output);
+
+#endif
